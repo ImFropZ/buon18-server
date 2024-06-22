4a2622f719f59ba5eb7d6e9f4f51f5c7b936c707
LimTangmeng <limtangmeng100@gmail.com>
2024-06-22 22:34:34 +0700

    feat: add list users api

--- controllers/user.go
+++ controllers/user.go
@@ -1,16 +1,58 @@
 package controllers
 
 import (
+	"server/models"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
+
+type PaginationQueryParams struct {
+	Offset int
+	Limit  int
+}
+
+type UserResponse struct {
+	ID    uint   `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Role  string `json:"role"`
+}
 
 type UserHandler struct {
 	DB *gorm.DB
 }
 
 func (handler *UserHandler) List(c *gin.Context) {
+	queryParams := PaginationQueryParams{
+		Offset: 0,
+		Limit:  10,
+	}
+
+	if offset, err := strconv.Atoi(c.Query("offset")); err == nil {
+		if offset < 0 {
+			offset = 0
+		}
+		queryParams.Offset = offset
+	}
+	if limit, err := strconv.Atoi(c.Query("limit")); err == nil {
+		if limit < 1 {
+			limit = 10
+		}
+		queryParams.Limit = limit
+	}
+
+	var users []UserResponse
+	result := handler.DB.Model(&models.User{}).Limit(queryParams.Limit).Offset(queryParams.Offset).Find(&users)
+	if result.Error != nil {
+		c.JSON(500, gin.H{
+			"error": "internal server error",
+		})
+		return
+	}
+
 	c.JSON(200, gin.H{
-		"message": "List users",
+		"users": users,
 	})
 }
