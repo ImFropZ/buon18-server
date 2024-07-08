2b7c17f5c51aaddea72cddf1cd4abdb2baca2d78
LimTangmeng <limtangmeng100@gmail.com>
2024-07-08 18:51:06 +0700

    fix: remove double authenticate from `accounts` routes

--- routes/account.go
+++ routes/account.go
@@ -11,9 +11,9 @@ import (
 func Account(e *gin.Engine, db *sql.DB) {
 	handler := controllers.AccountHandler{DB: db}
 
-	e.GET("/api/accounts", middlewares.Authenticate(db), handler.List)
-	e.GET("/api/accounts/:id", middlewares.Authenticate(db), handler.First)
-	e.POST("/api/accounts", middlewares.Authenticate(db), middlewares.Authorize(middlewares.Editor), handler.Create)
-	e.PATCH("/api/accounts/:id", middlewares.Authenticate(db), middlewares.Authorize(middlewares.Editor), handler.Update)
-	e.DELETE("/api/accounts/:id", middlewares.Authenticate(db), middlewares.Authorize(middlewares.Admin), handler.Delete)
+	e.GET("/api/accounts", handler.List)
+	e.GET("/api/accounts/:id", handler.First)
+	e.POST("/api/accounts", middlewares.Authorize(middlewares.Editor), handler.Create)
+	e.PATCH("/api/accounts/:id", middlewares.Authorize(middlewares.Editor), handler.Update)
+	e.DELETE("/api/accounts/:id", middlewares.Authorize(middlewares.Admin), handler.Delete)
 }
