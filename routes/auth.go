98f40ac819592117a59aa2b3eeae439a6a8da95a
LimTangmeng <limtangmeng100@gmail.com>
2024-08-07 18:44:05 +0700

    feat: add predefined permissions

--- routes/auth.go
+++ routes/auth.go
@@ -4,16 +4,18 @@ import (
 	"database/sql"
 	"server/controllers"
 	"server/middlewares"
+	"server/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Auth(e *gin.Engine, db *sql.DB) {
 	handler := controllers.AuthHandler{DB: db}
+	authPermissions := utils.PREDEFINED_PERMISSIONS.AUTH
 
-	e.GET("/api/auth/me", middlewares.Authenticate(db), middlewares.Authorize([]string{"VIEW_PROFILE"}), handler.Me)
+	e.GET("/api/auth/me", middlewares.Authenticate(db), middlewares.Authorize([]string{authPermissions.VIEW}), handler.Me)
 	e.POST("/api/auth/login", handler.Login)
 	e.POST("/api/auth/refresh-token", handler.RefreshToken)
-	e.POST("/api/auth/update-password", middlewares.Authenticate(db), middlewares.Authorize([]string{"UPDATE_PROFILE"}), handler.UpdatePassword)
-	e.PATCH("/api/auth/me", middlewares.Authenticate(db), middlewares.Authorize([]string{"UPDATE_PROFILE"}), handler.UpdateProfile)
+	e.POST("/api/auth/update-password", middlewares.Authenticate(db), middlewares.Authorize([]string{authPermissions.UPDATE}), handler.UpdatePassword)
+	e.PATCH("/api/auth/me", middlewares.Authenticate(db), middlewares.Authorize([]string{authPermissions.UPDATE}), handler.UpdateProfile)
 }
