d0d3395ba91f70ff2ae0dfa315eb98a93ae736bf
LimTangmeng <limtangmeng100@gmail.com>
2024-08-12 15:08:29 +0700

    chore: add test data for user and role

--- sql/data/01_seed-user.down.sql
+++ sql/data/01_seed-user.down.sql
@@ -1,3 +1,12 @@
 DELETE FROM "setting.user"
 WHERE
-    email ILIKE '%@dummy-data.com';
+    email ILIKE '%@dummy-data.com'
+    OR id BETWEEN 3 AND 5;
+
+DELETE FROM "setting.role_permission"
+WHERE
+    setting_role_id BETWEEN 3 AND 5;
+
+DELETE FROM "setting.role"
+WHERE
+    id BETWEEN 3 AND 5;
--- sql/data/01_seed-user.up.sql
+++ sql/data/01_seed-user.up.sql
@@ -1,5 +1,52 @@
 INSERT INTO
+    "setting.role" (id, name, description, cid, ctime, mid, mtime)
+VALUES
+    (
+        3,
+        'Setting Administrator',
+        'Full access to all settings',
+        1,
+        NOW(),
+        1,
+        NOW()
+    ), -- Admin permissions
+    (
+        4,
+        'Sales Administrator',
+        'Full access to all sales',
+        1,
+        NOW(),
+        1,
+        NOW()
+    ),
+    (
+        5,
+        'Accounting Administrator',
+        'Full access to all accounting',
+        1,
+        NOW(),
+        1,
+        NOW()
+    );
+
+INSERT INTO
+    "setting.role_permission" (
+        setting_role_id,
+        setting_permission_id,
+        cid,
+        ctime,
+        mid,
+        mtime
+    )
+VALUES
+    (3, 3, 1, NOW(), 1, NOW()), -- Admin permissions
+    (4, 4, 1, NOW(), 1, NOW()),
+    (5, 5, 1, NOW(), 1, NOW());
+
+-- User permissions
+INSERT INTO
     "setting.user" (
+        id,
         name,
         email,
         setting_role_id,
@@ -10,6 +57,37 @@ INSERT INTO
     )
 VALUES
     (
+        3,
+        'Setting Admin',
+        'setting@buon18.com',
+        3,
+        1,
+        NOW(),
+        1,
+        NOW()
+    ),
+    (
+        4,
+        'Sales Admin',
+        'sales@buon18.com',
+        4,
+        1,
+        NOW(),
+        1,
+        NOW()
+    ),
+    (
+        5,
+        'Accounting Admin',
+        'accounting@buon18.com',
+        5,
+        1,
+        NOW(),
+        1,
+        NOW()
+    ),
+    (
+        1000,
         'John Doe',
         'jd@dummy-data.com',
         2,
@@ -19,6 +97,7 @@ VALUES
         NOW()
     ), -- User permissions
     (
+        1001,
         'Jane Doe',
         'jad@dummy-data.com',
         2,
